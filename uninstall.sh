a0052a5d985ff618ef52170113cae19d21367838
nimugen <emerclaw@gmail.com>
2025-11-07 09:58:13 +0700

    fix script

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,13 @@
 
 sudo -v
 
-rm -f /usr/local/bin/lf
-rm -f /usr/local/man/man1/lf.1.gz
+man_path() {
+
+	manpath | cut -d ":" -f1
+
+}
+
+sudo rm -f /usr/local/bin/lf
+sudo rm -f $(man_path)/man1/lf.1.gz
 
 echo "Uninstalled Succesfully"
